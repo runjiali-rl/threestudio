27074c3834ef1f16d5ed0f13b9d5a595ccf283d1
runjiali-rl <runjiali@outlook.com>
2024-05-13 13:06:50 +0100

    add run baseline experiment bash command

---
+++ eval_scripts/run_baseline.sh
@@ -0,0 +1,11 @@
+#/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/baseline/mvdream-sd21-deepfloyd-0-1.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body"
+
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/baseline/mvdream-sd21-deepfloyd-0.2-0.8.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body"
+
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/baseline/mvdream-sd21-deepfloyd-0.4-0.6.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body"
+
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/baseline/mvdream-sd21-deepfloyd-0.6-0.4.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body"
+
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/baseline/mvdream-sd21-deepfloyd-0.8-0.2.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body"
+
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/baseline/mvdream-sd21-deepfloyd-1-0.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body"
---
+++ eval_scripts/run_compositional_baseline.sh
@@ -0,0 +1,11 @@
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/compositional_baseline/mvdream-sd21-deepfloyd-compositional-0-1.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body composition 0 1"
+
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/compositional_baseline/mvdream-sd21-deepfloyd-compositional-0.2-0.8.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body composition 2 8"
+
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/compositional_baseline/mvdream-sd21-deepfloyd-compositional-0.4-0.6.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body composition 4 6"
+
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/compositional_baseline/mvdream-sd21-deepfloyd-compositional-0.6-0.4.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body composition 6 4"
+
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/compositional_baseline/mvdream-sd21-deepfloyd-compositional-0.8-0.2.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body composition 8 2"
+
+/homes/55/runjia/anaconda3/envs/threestudio/bin/python launch.py --config custom/threestudio-mvdream/configs/compositional_baseline/mvdream-sd21-deepfloyd-compositional-1-0.yaml --gpu 0 --train system.prompt_processor.prompt="a photo of a Lion with a sheep body composition 1 0"
