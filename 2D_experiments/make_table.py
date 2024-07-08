ff52bfdfd20473137850556e3869e55ddf3ba476
runjiali-rl <runjiali@outlook.com>
2024-07-08 13:06:11 +0100

    update results

--- 2D_experiments/make_table.py
+++ 2D_experiments/make_table.py
@@ -5,26 +5,34 @@ import json
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Make table of results')
-    parser.add_argument('--results_dir', default="2D_experiments/results", type=str, help='Path to directory containing results')
-    parser.add_argument('--output', default="2D_experiments/results/results_table.json", type=str, help='Path to output file')
-    parser.add_argument('--iterative', default=False, action='store_true', help='Whether it is the iterative version of the experiment')
+    parser.add_argument('--results_dir', default="2D_experiments/results/iterative/part", type=str, help='Path to directory containing results')
+    parser.add_argument('--output', default="2D_experiments/results/iterative/part/results_table.json", type=str, help='Path to output file')
+    parser.add_argument('--iterative', default=True, action='store_true', help='Whether it is the iterative version of the experiment')
     return parser.parse_args()
 
 def main():
     args = parse_args()
     results_dir = args.results_dir
     subdirs = os.listdir(results_dir)
+   
+
+        # sort the subdirs so that the results are in the correct order
+     
     table_results = {}
 
     subdirs = [os.path.join(results_dir, subdir) for subdir in subdirs]
-    for subdir in subdirs:
-        if "iterative" in subdir:
+    for subdir in subdirs :
+        if "iterative" in subdir and not args.iterative:
             continue
         if subdir.endswith('.json'):
             continue
         subdir_key = subdir.split('/')[-1]
         table_results[subdir_key] = {}
         model_results = os.listdir(subdir)
+        # remove the json file from the list of model results
+        model_results = [model_result for model_result in model_results if not model_result.endswith('.json')]
+        # sort the model results so that they are in the correct order
+        model_results = sorted(model_results)
         for model_result in model_results:
             model_result_key = model_result.split('.')[0]
             table_results[subdir_key][model_result_key] = None
